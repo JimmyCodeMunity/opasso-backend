a25633ab31819c3ee6eef19971bed6574b0a008d
Jimmy <96749667+JimmyCodeMunity@users.noreply.github.com>
2023-12-18 13:05:10 +0300

    Update UserRoute.js

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const User = require('../models/UserModel');
-const { getAllUsers, getAllSellers, Login,createUser } = require('../controllers/UserController');
+const { getAllUsers, getAllSellers, Login,createUser,getAllUsersByEmail } = require('../controllers/UserController');
 
 const router = express.Router();
 router.use(express.json());
@@ -14,5 +14,7 @@ router.get('/usersdata',getAllUsers);
 router.post('/login',Login);
 
 router.post('/register',createUser);
+
+router.get('/usersdata/:email',getAllUsersByEmail);
 
 module.exports = router;
