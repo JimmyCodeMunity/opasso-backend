f8494025b73b14dfaca51e43997c8e6e7d8be6e7
Jimmy <96749667+JimmyCodeMunity@users.noreply.github.com>
2023-12-19 08:40:24 +0300

    Update ProductRoute.js

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -20,7 +20,7 @@ router.get('/productlistcategory/:category',getProductByCategory);
 router.get('/productlist/:shopId',getProductByShopId);
 
 //search products
-router.get('/search', searchProducts);
+router.post('/search', searchProducts);
 
 
 module.exports = router;
